a2a4989c1f3b7a35028e3e14f66313a3a86f93da
haukevan <haukevan@gmail.com>
2023-06-12 22:09:27 -0700

    created preview page for save image and modal

--- app/page.jsx
+++ app/page.jsx
@@ -20,25 +20,38 @@ export default function Home() {
   const [background, setBackground] = useState("");
   const [newImage, setnewImage] = useState(null);
 
-  // useEffect(() => {
-  //   console.log("new Image: " + newImage);
-  // }, [newImage]);
-
-  //iphone stats
+  //iphone stats --NOTE: image dimensions are multiplied by 3 when using dataURL objects
   const phoneList = [
     {
       id: "iphone13Default",
       width: "428px",
       height: "926px",
+      topWidgets: "243px",
+      widgetWidth: "386px",
+      widgetHeight: "93px",
+      widgetBorderRadius: "20px",
     },
     {
       id: "iphone13ProMax",
-      // width: "1284px",
-      // height: "2778px",
-      width: "428px",
-      height: "926px",
+      width: "428px", //"1284px"
+      height: "926px", // "2778px"
+      topWidgets: "243px",
+      widgetWidth: "386px",
+      widgetHeight: "93px",
+      widgetBorderRadius: "20px",
     },
   ];
+
+  //set initial scale of preview phone on load
+  useEffect(() => {
+    const exportImage = document.getElementById("exportImage");
+    exportImage.style.width = phoneList.find(
+      (phoneSelected) => phoneSelected.id === "iphone13Default"
+    ).width;
+    exportImage.style.height = phoneList.find(
+      (phoneSelected) => phoneSelected.id === "iphone13Default"
+    ).height;
+  });
 
   const handleChangeComplete = (color) => {
     setBackground(color.hex);
@@ -60,6 +73,8 @@ export default function Home() {
   const prepareImage = async () => {
     //set full resolution styles before export, then set back to original for preview
     const exportImage = document.getElementById("exportImage");
+
+    //set wallpaper image dimensions
     exportImage.style.width = phoneList.find(
       (phoneSelected) => phoneSelected.id === phone
     ).width;
@@ -67,10 +82,27 @@ export default function Home() {
       (phoneSelected) => phoneSelected.id === phone
     ).height;
 
+    //set widget styles
+    const exportWidget = document.getElementById("widgets");
+    exportWidget.style.top = phoneList.find(
+      (phoneSelected) => phoneSelected.id === phone
+    ).topWidgets;
+    exportWidget.style.width = phoneList.find(
+      (phoneSelected) => phoneSelected.id === phone
+    ).widgetWidth;
+    exportWidget.style.height = phoneList.find(
+      (phoneSelected) => phoneSelected.id === phone
+    ).widgetHeight;
+    exportWidget.style.borderRadius = phoneList.find(
+      (phoneSelected) => phoneSelected.id === phone
+    ).widgetBorderRadius;
+
     console.log("width:" + exportImage.style.width);
 
     //save image as objectURL and set to newImage, this will be updated on the newImage displayer
     setnewImage(await exportAsImage(exportRef.current, "test"));
+
+    document.getElementById("saveImageBox").style.visibility = "visible";
 
     //set styles back for UI viewing
     exportImage.style.width = phoneList.find(
@@ -100,8 +132,23 @@ export default function Home() {
     setPhone((currentPhone) => event.target.value);
   };
 
+  //save preview functions
+  const closeSavePreview = () => {
+    document.getElementById("saveImageBox").style.visibility = "hidden";
+  };
+
+  const openHelpModal = () => {
+    document.querySelector("[data-modal]").showModal();
+  };
+
+  const closeHelpModal = () => {
+    document.querySelector("[data-modal]").close();
+  };
+
+  //-----------------------RETURN----------------------------//
+
   return (
-    <main className=" gap-2 w-full h-fit overflow-hidden flex flex-col items-center justify-start bg-gradient-to-b from-blue-600 to-cyan-600">
+    <main className=" relative gap-2 w-full h-fit overflow-hidden flex flex-col items-center justify-start bg-gradient-to-b from-blue-600 to-cyan-600">
       <h1>Wallpaper Creator</h1>
       <div
         className="uploadButton w-fit h-fit p-3 flex flex-row justify-center align-middle rounded-xl bg-emerald-800 text-slate-100 font-bold font-large"
@@ -125,7 +172,7 @@ export default function Home() {
       <div
         ref={exportRef}
         id="exportImage"
-        className=" w-[428px] h-[926px] overflow-hidden relative flex align-middle justify-center rounded-3xl"
+        className="buildImage w-[428px] h-[926px] overflow-hidden relative flex align-middle justify-center rounded-3xl"
       >
         {image ? (
           <div
@@ -177,17 +224,31 @@ export default function Home() {
         color={background}
         onChangeComplete={handleChangeComplete}
       />
-      {newImage ? (
-        <img alt="" src={newImage} className="save-img" />
-      ) : (
-        <img
-          width="200"
-          height="200"
-          alt=""
-          src="/assets/images/iphone13.png"
-          className="bkg-img"
-        />
-      )}
+      <div id="saveImageBox" className="saveImageBox">
+        <div className="saveImageBoxHeader">
+          <div onClick={closeSavePreview}>Close</div>
+          <div data-open-modal onClick={openHelpModal}>
+            Help
+          </div>
+        </div>
+        {newImage ? (
+          <img id="" alt="" src={newImage} className="save-img" />
+        ) : (
+          <img
+            width="200"
+            height="200"
+            alt=""
+            src="/assets/images/iphone13.png"
+            className="bkg-img"
+          />
+        )}
+      </div>
+      <dialog data-modal>
+        <div>Modals</div>
+        <button data-close-modal onClick={closeHelpModal}>
+          Close
+        </button>
+      </dialog>
     </main>
   );
 }

061358e212622dc030e050ce41fb0d55caffc345
haukevan <haukevan@gmail.com>
2023-06-13 21:45:31 -0700

    Updates for design menu

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,18 @@
 
 import { useRef, useState, useEffect } from "react";
 import exportAsImage from "../utils/exportAsImage";
-import { SketchPicker } from "react-color";
+import { ChromePicker } from "react-color";
+import rgbHex from "rgb-hex";
 
 import Image from "next/image";
 import upload from "../public/assets/icons/upload.svg";
+import leftJustify from "../public/assets/icons/align-left.svg";
+import rightJustify from "../public/assets/icons/align-right.svg";
+import centerJustify from "../public/assets/icons/align-center.svg";
+import close from "../public/assets/icons/close.svg";
+import align from "../public/assets/icons/align.svg";
+import palette from "../public/assets/icons/palette.svg";
+import borderRadius from "../public/assets/icons/border-radius.svg";
 
 export default function Home() {
   //export image button ref
@@ -17,8 +25,9 @@ export default function Home() {
   //upload image state holder from user
   const [image, setImage] = useState(null);
   const [phone, setPhone] = useState(null);
-  const [background, setBackground] = useState("");
+  const [background, setBackground] = useState("rbga(0, 0, 0, 0)");
   const [newImage, setnewImage] = useState(null);
+  const [menuSelect, setmenuSelect] = useState(0); //default no show items
 
   //iphone stats --NOTE: image dimensions are multiplied by 3 when using dataURL objects
   const phoneList = [
@@ -57,8 +66,10 @@ export default function Home() {
     document.getElementById("widgets").style.background = background;
   }, [background]);
 
-  const handleChangeComplete = (color) => {
-    setBackground(color.hex);
+  const handleChangeComplete = (c) => {
+    //setBackground(color.rgb);
+    //console.log(background);
+    setBackground("#" + rgbHex(c.rgb.r, c.rgb.g, c.rgb.b, c.rgb.a));
   };
 
   const handleUploadClick = () => {
@@ -157,30 +168,54 @@ export default function Home() {
     document.getElementById("designBoxBottom").style.visibility = "hidden";
   };
 
+  const menuSelectHandler = (selected) => {
+    setmenuSelect(selected);
+  };
+
   //-----------------------RETURN----------------------------//
 
   return (
     <main className=" relative gap-2 w-full h-fit overflow-hidden flex flex-col items-center justify-start bg-gradient-to-b from-blue-600 to-cyan-600">
-      <h1>Wallpaper Creator</h1>
-      <div
-        className="uploadButton w-fit h-fit p-3 flex flex-row justify-center align-middle rounded-xl bg-emerald-800 text-slate-100 font-bold font-large"
-        onClick={handleUploadClick}
-      >
-        <Image
-          className="uploadImage"
-          priority
-          src={upload}
-          alt="Upload an image"
-        />
-        <h3 className=" text-4xl font-normal">Upload</h3>
-        <input
-          type="file"
-          onChange={onImageChange}
-          className="filetype"
-          ref={inputRef}
-          style={{ display: "none" }}
-        />
+      <h1 className=" text-white text-4xl pt-2">Wallpaper Creator</h1>
+      <div className=" flex flex-row justify-center align-middle p-1">
+        <div
+          className="uploadButton w-fit h-fit m-1 p-3 flex flex-row justify-center align-middle rounded-xl bg-emerald-800 text-slate-100 font-bold font-medium"
+          onClick={handleUploadClick}
+        >
+          <Image
+            className="uploadImage"
+            priority
+            src={upload}
+            alt="Upload an image"
+          />
+          <h3 className=" text-xl font-normal">Upload</h3>
+          <input
+            type="file"
+            onChange={onImageChange}
+            className="filetype"
+            ref={inputRef}
+            style={{ display: "none" }}
+          />
+        </div>
+        <div
+          className="uploadButton w-fit h-fit m-1 p-3 flex flex-row justify-center align-middle rounded-xl bg-emerald-800 text-slate-100 font-bold font-medium"
+          onClick={prepareImage}
+        >
+          <h3 className=" text-xl font-normal">Download</h3>
+        </div>
       </div>
+      <select
+        onChange={handleChangePhone}
+        id="phoneSelect"
+        defaultValue={"select"}
+      >
+        <option value="select" disabled>
+          Phone Model
+        </option>
+        <option value="13reg">iPhone 13</option>
+        <option value="13pro">iPhone 13 Pro</option>
+        <option value="iphone13ProMax">iPhone 13 Pro Max</option>
+      </select>
       <div
         ref={exportRef}
         id="exportImage"
@@ -204,43 +239,83 @@ export default function Home() {
         )}
         <div className="widgets" id="widgets" onClick={openDesignBottom}></div>
       </div>
-      <button onClick={prepareImage}>Capture Image</button>
-      <select
-        onChange={handleChangePhone}
-        id="phoneSelect"
-        defaultValue={"select"}
-      >
-        <option value="select" disabled>
-          Choose here
-        </option>
-        <option value="13reg">iPhone 13</option>
-        <option value="13pro">iPhone 13 Pro</option>
-        <option value="iphone13ProMax">iPhone 13 Pro Max</option>
-      </select>
 
       {/* Design Menu to open up bottom up or top down for elements */}
       <div className="designBoxBottom" id="designBoxBottom">
         <div className=" flex flex-row align-middle justify-between">
           <div>Menu Area</div>
           <div id="closeDesignBoxBottom" onClick={closeDesignBoxBottom}>
-            Close
+            <Image src={close} alt="Close design menu" />
           </div>
         </div>
-        <div className=" flex justify-center align-middle flex-col">
-          <button id="leftBtn" onClick={alignClick}>
-            Left
-          </button>
-          <button id="centerBtn" onClick={alignClick}>
-            Center
-          </button>
-          <button id="rightBtn" onClick={alignClick}>
-            Right
-          </button>
-          <SketchPicker
-            color={background}
-            onChangeComplete={handleChangeComplete}
+        <div
+          id="menuSelect"
+          className=" flex flex-row align-middle justify-center p-2"
+        >
+          <input
+            type="radio"
+            className="radio_item"
+            value="AlignImage"
+            name="item"
+            id="radio1"
+            checked={menuSelect === 1}
+            onClick={(e) => menuSelectHandler(1)}
           />
+          <label className="label_item" for="radio1">
+            <Image src={align} alt="Align image" />
+          </label>
+          <input
+            type="radio"
+            className="radio_item"
+            value="SelectColor"
+            name="item"
+            id="radio2"
+            checked={menuSelect === 2}
+            onClick={(e) => menuSelectHandler(2)}
+          />
+          <label className="label_item" for="radio2">
+            <Image src={palette} alt="Pick color" />
+          </label>
+          <input
+            type="radio"
+            className="radio_item"
+            value="BorderRadius"
+            name="item"
+            id="radio3"
+            checked={menuSelect === 3}
+            onClick={(e) => menuSelectHandler(3)}
+          />
+          <label className="label_item" for="radio3">
+            <Image src={borderRadius} alt="Border Radius" />
+          </label>
         </div>
+        {menuSelect === 1 && (
+          <div className=" flex justify-center align-middle flex-row">
+            <div onClick={alignClick}>
+              <Image id="leftBtn" src={leftJustify} alt="Align image left" />
+            </div>
+            <div onClick={alignClick}>
+              <Image
+                id="centerBtn"
+                src={centerJustify}
+                alt="Align image center"
+              />
+            </div>
+            <div onClick={alignClick}>
+              <Image id="rightBtn" src={rightJustify} alt="Align image right" />
+            </div>
+          </div>
+        )}
+        {menuSelect === 2 && (
+          <div>
+            <ChromePicker
+              width={"200px"}
+              color={background}
+              onChangeComplete={handleChangeComplete}
+            />
+          </div>
+        )}
+        {menuSelect === 3 && <div>Border Radius Selector</div>}
       </div>
 
       {/* Save box that will pop up fixed ontop of design area */}
